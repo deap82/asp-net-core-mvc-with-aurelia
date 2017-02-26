53501565eba778c8f279d3f8b3ea2c91f28268f6
Dan Pettersson <dan82pettersson@gmail.com>
2017-02-26 15:13:44 +0100

    Removed Application Insights.

--- src/FooBar.Web/Program.cs
+++ src/FooBar.Web/Program.cs
@@ -16,7 +16,6 @@ namespace FooBar.Web
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
