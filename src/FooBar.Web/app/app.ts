21ee49bbf0f62283cf52118e4ea724d65cc3c6ee
Dan Pettersson <dan82pettersson@gmail.com>
2017-05-04 17:55:57 +0200

    Setting up navigation between server side rendered mvc actions.

--- src/FooBar.Web/app/app.ts
+++ src/FooBar.Web/app/app.ts
@@ -1,3 +1,15 @@
-﻿export class App {
-	message: string = 'Hello World!';
+﻿import { Router, RouterConfiguration } from 'aurelia-router';
+
+export class App {
+	configureRouter(config: RouterConfiguration, router: Router) {
+		config.map(
+		[
+			{ route: '', redirect: 'Home/Start' },
+			{
+				name: 'MvcRoute',
+				route: ':mvcController/:mvcAction/:id?',
+				moduleId: 'app/routing/mvc-route-navigation/mvc-route'
+			}
+		]);
+	}
 }
---
+++ src/FooBar.Web/app/routing/mvc-route-navigation/mvc-route.ts
@@ -0,0 +1,35 @@
+﻿import 'whatwg-fetch';
+import { autoinject } from 'aurelia-framework';
+import { HttpClient } from 'aurelia-fetch-client';
+
+@autoinject
+export class MvcRoute {
+	html: string;
+
+	constructor(private httpClient: HttpClient) {
+	}
+
+	activate(params: any) {
+		let url = this.resolveMvcUrl(params);
+		this.loadHtml(url).then(html => {
+			this.html = html;
+		});
+	}
+
+	private resolveMvcUrl(params: any): string {
+		let url = `/${params.mvcController}/${params.mvcAction}/${(params.id || '')}`;
+		delete params.mvcController;
+		delete params.mvcAction;
+		delete params.id;
+		var queryString = $.param(params);
+		if (queryString) {
+			url += `?${queryString}`;
+		}
+		return url;
+	}
+
+	private loadHtml(mvcRoute: string): Promise<string> {
+		const result = this.httpClient.fetch(mvcRoute).then(response => response.text());
+		return result;
+	}
+}
